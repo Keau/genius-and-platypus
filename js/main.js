3c335ab62d28480ded58861d6569822fc15491f6
Keau <keau@outlook.com>
2016-02-08 00:53:46 +0100

    feat: change overall look

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).on('scroll', function(){
-    if ($('#main-nav')[0].offsetTop < $(document).scrollTop()){
-        $("#main-nav").css({position: "fixed", top:0, left:0, width: "100%"});
-    }
-    if ($(document).scrollTop() < $("#main-nav-position-saver")[0].offsetTop){
-        $("#main-nav").css({position: "static", top: 0});
-    }
-});
